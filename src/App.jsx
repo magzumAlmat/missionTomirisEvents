bd6778393b00b4480e8e3f432d14e6915f95038b
magzumAlmat <almat.magzum@gmail.com>
2026-09-17 10:17:17 +0300

    feat: removed /progress page entirely

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Link, Navigate } from "react-router-dom";
 import { QUEST } from "./questConfig.js";
 import Start from "./pages/Start.jsx";
 import Station from "./pages/Station.jsx";
-import Progress from "./pages/Progress.jsx";
+
 import Admin from "./pages/Admin.jsx";
 import AdminProgress from "./pages/AdminProgress.jsx";
 import Register from "./pages/Register.jsx";
@@ -23,7 +23,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/reg" element={<Register />} />
         <Route path="/s/:id" element={<Station />} />
-        <Route path="/progress" element={<Progress />} />
+
         <Route path="/admin" element={<Admin />} />
         <Route path="/admin/progress" element={<AdminProgress />} />
         <Route path="*" element={<Navigate to="/" replace />} />
